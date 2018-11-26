2187d74a09dee6d42aa89e29d6b84ec936a152c5
omar marzougui <marzouguiomar@hotmail.fr>
2018-11-26 16:30:38 +0100

    Exercice1

---
+++ javascript/lesson2/exercise1/main.js
@@ -0,0 +1,10 @@
+
+ var heure = 12
+
+ if (heure < 10) {
+     console.log("c'est le matin");
+ } else if (heure < 20) {
+     console.log("c'est l'apres-midi");
+ } else {
+     console.log("c'est le soir");
+ }
