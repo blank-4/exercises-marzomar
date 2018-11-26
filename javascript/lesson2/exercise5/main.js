0356458f205f53efe580e09f7fcf85cc3e6b7537
omar marzougui <marzouguiomar@hotmail.fr>
2018-11-26 17:28:03 +0100

    Exercise5

---
+++ javascript/lesson2/exercise5/main.js
@@ -0,0 +1,20 @@
+/* Pour commencer on veut que vous affichiez dans la console tout les nombres compris entre 1 et un nombre de votre choix avec une boucle for.
+
+* Ensuite on veut que vous vous arrangiez pour que "fizz" s'affiche dans la console a la place de tout multiples de 7 et que les multiples de 5 soient remplacés par "buzz" . (Pensez qu'il est possible d'utiliser un "if else" dans une boucle for) .
+
+c'est ce qu'on appelle un "fizzbuzz" ! ( sans blague ... )
+*/
+
+for (var i =1; i <= 50; i++) {
+   if (i%7==0) {
+       console.log("fizz");
+
+   } else if (i%5==0) {
+       console.log("buzz");
+    }
+    else {
+        console.log(i);
+    }
+}
+    
+      
