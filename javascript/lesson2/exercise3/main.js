b34422166be5df4583309c7305488bbf31c7ce7b
omar marzougui <marzouguiomar@hotmail.fr>
2018-11-26 17:03:08 +0100

    exercice3

---
+++ javascript/lesson2/exercise3/main.js
@@ -0,0 +1,13 @@
+/*Après "if else", après "for", c'est maintenant au tour de "while" !
+
+On veut une boucle qui affiche tout les nombres de zéro a mille dans la console (oui on aime afficher des nombres ici).
+
+* Déclarez une variable "n" qui vaudra 0
+et arrangez vous pour obtenir le résultat attendu à l'aide de "while".*/
+
+var n = 0;
+
+while (n<=1000) {
+    console.log(n);
+    n++;
+}
