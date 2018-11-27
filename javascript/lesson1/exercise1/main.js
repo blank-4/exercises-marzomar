3ba19cdb05128f66f154481ab2ebf245193f938a
marzomar <44778655+marzomar@users.noreply.github.com>
2018-11-27 12:14:02 +0100

    exercice 1 & 2 (#11)

---
+++ javascript/lesson1/exercise1/main.js
@@ -0,0 +1,17 @@
+var toto = "bonjour";
+console.log(toto);
+
+toto = 20+100;
+console.log(toto);
+
+toto = 50-25;
+console.log(toto);
+
+toto = 2*4;
+console.log(toto);
+
+toto = 8/2;
+console.log(toto);
+
+toto = 14%2;
+console.log(toto);
---
+++ javascript/lesson1/exercise2/main.js
@@ -0,0 +1,22 @@
+var a = 100;
+var b = 100;
+
+var c = a+b;
+console.log(c);
+
+var d = a-b;
+console.log(d);
+
+var e = a*b;
+console.log(e);
+
+var f = a/b;
+console.log(f);
+
+var g = a%g;
+console.log(g)
+
+var h = "b";
+
+var i = a+h;
+console.log(i);
