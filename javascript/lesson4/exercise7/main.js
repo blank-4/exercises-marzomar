505f916fa29e2a0006453e97c2ed5e1f85364c49
omar marzougui <marzouguiomar@hotmail.fr>
2018-12-04 15:34:30 +0100

    exercice 7

--- javascript/lesson4/exercise7/main.js
+++ javascript/lesson4/exercise7/main.js
@@ -3,7 +3,7 @@ function NbJoursMois(mois, annee) {
     if ((annee%4 == 0 && annee%100 != 0) || annee%400 == 0) 
     {lgMois[2] += 1;
     }
-    return lgMois[mois]; // 0 < mois <11
+    return lgMois[mois]; 
     }
 
     console.log(NbJoursMois(2, 2008));
